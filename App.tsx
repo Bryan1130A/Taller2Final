9438d5b6a0679b2c479afa3d0b21bcbf86abebd2
Bryan1130A <leonaprot44@gmail.com>
2025-08-03 21:48:23 -0500

    Primer commit: Proyecto App de Login

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './navigator/StackNavigator';
 import { Pantalla1Screens } from './src/Screen/Pantalla1Screans';
-import { Pantalla2Screens } from './src/Screen/pantalla2.Screans';
+
 
 export default function App() {
   return (
